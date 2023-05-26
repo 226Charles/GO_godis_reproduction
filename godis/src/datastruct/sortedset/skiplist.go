aee86fe85ed403fb6e46c8e47a346aa30f718c45
226Charles <953159142@qq.com>
2023-05-26 16:45:56 +0800

    update sortedset init

---
+++ godis/src/datastruct/sortedset/skiplist.go
@@ -0,0 +1,117 @@
+package sortedset
+
+import (
+	"math/bits"
+	"math/rand"
+)
+
+const maxLevel = 16
+
+type Element struct {
+	Member string
+	Score  float64
+}
+
+type Level struct {
+	forward *node
+	span    int64
+}
+
+type node struct {
+	Element
+	backward *node
+	level    []*Level
+}
+
+type skiplist struct {
+	header *node
+	tail   *node
+	length int64
+	level  int16
+}
+
+func makeNode(level int16, score float64, member string) *node {
+	n := &node{
+		Element: Element{
+			Score:  score,
+			Member: member,
+		},
+		level: make([]*Level, level),
+	}
+	for i := range n.level {
+		n.level[i] = new(Level)
+	}
+	return n
+}
+
+func makeSkiplist() *skiplist {
+	return &skiplist{
+		level:  1,
+		header: makeNode(maxLevel, 0, ""),
+	}
+}
+
+func randomLevel() int16 {
+	total := uint64(1)<<uint64(maxLevel) - 1
+	k := rand.Uint64() % total
+	return maxLevel - int16(bits.Len64(k+1)) + 1
+}
+
+func (skiplist *skiplist) insert(member string, score float64) *node {
+	update := make([]*node, maxLevel)
+	rank := make([]int64, maxLevel)
+
+	node := skiplist.header
+	for i := skiplist.level - 1; i >= 0; i-- {
+		if i == skiplist.level-1 {
+			rank[i] = 0
+		} else {
+			rank[i] = rank[i+1]
+		}
+		if node.level[i] != nil {
+			for node.level[i].forward != nil &&
+				(node.level[i].forward.Score < score ||
+					(node.level[i].forward.Score == score && node.level[i].forward.Member < member)) {
+				rank[i] += node.level[i].span
+				node = node.level[i].forward
+			}
+		}
+		update[i] = node
+	}
+
+	level := randomLevel()
+	if level > skiplist.level {
+		for i := skiplist.level; i < level; i++ {
+			rank[i] = 0
+			update[i] = skiplist.header
+			update[i].level[i].span = skiplist.length
+		}
+		skiplist.level = level
+	}
+
+	node = makeNode(level, score, member)
+	for i := int16(0); i < level; i++ {
+		node.level[i].forward = update[i].level[i].forward
+		update[i].level[i].forward = node
+
+		node.level[i].span = update[i].level[i].span - (rank[0] - rank[i])
+		update[i].level[i].span = (rank[0] - rank[i] + 1)
+	}
+
+	for i := level; i < skiplist.level; i++ {
+		update[i].level[i].span++
+	}
+
+	if update[0] == skiplist.header {
+		node.backward = nil
+	} else {
+		node.backward = update[0]
+	}
+	if node.level[0].forward != nil {
+		node.level[0].forward.backward = node
+	} else {
+		skiplist.tail = node
+	}
+	skiplist.length++
+	return node
+}
