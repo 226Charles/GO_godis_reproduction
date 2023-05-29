70cfe3ab3bab0cb5c2d66d0f3e64017affbec296
Seu-Acid <56780530+226Charles@users.noreply.github.com>
2023-05-29 16:36:41 +0800

    add skiplist base
    跳表基本部分

--- godis/src/datastruct/sortedset/skiplist.go
+++ godis/src/datastruct/sortedset/skiplist.go
@@ -1,5 +1,8 @@
 package sortedset
 
+/*理解跳表的概念就简单
+ */
+
 import (
 	"math/bits"
 	"math/rand"
@@ -114,4 +117,182 @@ func (skiplist *skiplist) insert(member string, score float64) *node {
 	}
 	skiplist.length++
 	return node
+}
+
+func (skiplist *skiplist) removeNode(node *node, update []*node) {
+	for i := int16(0); i < skiplist.level; i++ {
+		if update[i].level[i].forward == node {
+			update[i].level[i].span += node.level[i].span - 1
+			update[i].level[i].forward = node.level[i].forward
+		} else {
+			update[i].level[i].span--
+		}
+	}
+	if node.level[0].forward != nil {
+		node.level[0].forward.backward = node.backward
+	} else {
+		skiplist.tail = node.backward
+	}
+	for skiplist.level > 1 && skiplist.header.level[skiplist.level-1].forward == nil {
+		skiplist.level--
+	}
+	skiplist.length--
+}
+
+func (skiplist *skiplist) remove(member string, score float64) bool {
+	update := make([]*node, maxLevel)
+	node := skiplist.header
+	for i := skiplist.level - 1; i >= 0; i-- {
+		for node.level[i].forward != nil &&
+			(node.level[i].forward.Score < score ||
+				(node.level[i].forward.Score == score &&
+					node.level[i].forward.Member == member)) {
+			node = node.level[i].forward
+		}
+		update[i] = node
+	}
+	node = node.level[0].forward
+	if node != nil && score == node.Score && node.Member == member {
+		skiplist.removeNode(node, update)
+		return true
+	}
+	return false
+}
+
+func (skiplist *skiplist) getRank(member string, score float64) int64 {
+	var rank int64 = 0
+	x := skiplist.header
+	for i := skiplist.level - 1; i >= 0; i-- {
+		for x.level[i].forward != nil &&
+			(x.level[i].forward.Score < score ||
+				(x.level[i].forward.Score == score &&
+					x.level[i].forward.Member <= member)) {
+			rank += x.level[i].span
+			x = x.level[i].forward
+		}
+		if x.Member == member {
+			return rank
+		}
+	}
+	return 0
+}
+
+func (skiplist *skiplist) getByRank(rank int64) *node {
+	var i int64 = 0
+	n := skiplist.header
+	for level := skiplist.level - 1; level >= 0; i-- {
+		for n.level[level].forward != nil && (i+n.level[level].span) <= rank {
+			i += n.level[level].span
+			n = n.level[level].forward
+		}
+		if i == rank {
+			return n
+		}
+	}
+	return nil
+}
+
+func (skiplist *skiplist) hasInRange(min *ScoreBorder, max *ScoreBorder) bool {
+	if min.Value > max.Value || (min.Value == max.Value && (min.Exclude || max.Exclude)) {
+		return false
+	}
+	n := skiplist.tail
+	if n == nil || !min.less(n.Score) {
+		return false
+	}
+	n = skiplist.header.level[0].forward
+	if n == nil || !max.greater(n.Score) {
+		return false
+	}
+	return true
+}
+
+func (skiplist *skiplist) getFirstInScoreRange(min *ScoreBorder, max *ScoreBorder) *node {
+	if !skiplist.hasInRange(min, max) {
+		return nil
+	}
+	n := skiplist.header
+	for level := skiplist.level - 1; level >= 0; level-- {
+		for n.level[level].forward != nil && !min.less(n.level[level].forward.Score) {
+			n = n.level[level].forward
+		}
+	}
+	n = n.level[0].forward
+	if !max.greater(n.Score) {
+		return nil
+	}
+	return n
+}
+
+func (skiplist *skiplist) getLastInScoreRange(min *ScoreBorder, max *ScoreBorder) *node {
+	if !skiplist.hasInRange(min, max) {
+		return nil
+	}
+	n := skiplist.header
+	for level := skiplist.level - 1; level >= 0; level-- {
+		for n.level[level].forward != nil && max.greater(n.level[level].forward.Score) {
+			n = n.level[level].forward
+		}
+	}
+	if !min.less(n.Score) {
+		return nil
+	}
+	return n
+}
+
+func (skiplist *skiplist) RemoveRangeByScore(min *ScoreBorder, max *ScoreBorder, limit int) (removed []*Element) {
+	update := make([]*node, maxLevel)
+	removed = make([]*Element, 0)
+	node := skiplist.header
+	for i := skiplist.level - 1; i >= 0; i-- {
+		for node.level[i].forward != nil {
+			if min.less(node.level[i].forward.Score) {
+				break
+			}
+			node = node.level[i].forward
+		}
+		update[i] = node
+	}
+	node = node.level[0].forward
+
+	for node != nil {
+		if !max.greater(node.Score) {
+			break
+		}
+		next := node.level[0].forward
+		removedElement := node.Element
+		removed = append(removed, &removedElement)
+		skiplist.removeNode(node, update)
+		if limit > 0 && len(removed) == limit {
+			break
+		}
+		node = next
+	}
+	return removed
+}
+
+func (skiplist *skiplist) RemoveRangeByRank(start int64, stop int64) (removed []*Element) {
+	var i int64 = 0
+	update := make([]*node, maxLevel)
+	removed = make([]*Element, 0)
+	node := skiplist.header
+	for level := skiplist.level - 1; level >= 0; level-- {
+		for node.level[level].forward != nil && (i+node.level[level].span) < start {
+			i += node.level[level].span
+			node = node.level[level].forward
+		}
+		update[level] = node
+	}
+	i++
+	node = node.level[0].forward
+
+	for node != nil && i < stop {
+		next := node.level[0].forward
+		removedElement := node.Element
+		removed = append(removed, &removedElement)
+		skiplist.removeNode(node, update)
+		node = next
+		i++
+	}
+	return removed
 }
